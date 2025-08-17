748183103dc115f23a3f73ae5aa90ea4c63a6b98
abunaim34 <farhanadnanfarabi@gmail.com>
2025-08-17 19:53:38 +0600

    this is about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,11 @@
+
+const About = () => {
+    return (
+        <div>
+            <h1 className="text-2xl">This is about page</h1>
+            <p className="mt-4 text-lg">Here you can find information about our application.</p>
+        </div>
+    );
+};
+
+export default About;
