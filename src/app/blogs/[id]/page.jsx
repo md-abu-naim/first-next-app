d900bca9e513a1d7253832c62889e04b84cd7a7d
abunaim34 <farhanadnanfarabi@gmail.com>
2025-08-18 21:41:44 +0600

    meta data

--- src/app/blogs/[id]/page.jsx
+++ src/app/blogs/[id]/page.jsx
@@ -1,22 +1,22 @@
 
-export const getBlog = async(id) => {
+export const getBlog = async (id) => {
     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
-    const blog = await res.json() 
+    const blog = await res.json()
     return blog
 }
 
-export const generateMetadata = async({params}) => {
+export const generateMetadata = async ({ params }) => {
     const id = (await params).id
-    const blog =await getBlog(id)
+    const blog = await getBlog(id)
 
-    return{
+    return {
         title: blog.title,
         description: blog.body
     }
- 
+
 }
 
-const BlogsDetails = async({params}) => {
+const BlogsDetails = async ({ params }) => {
     // const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.id}`);
     // const blog = await res.json() 
     const blog = await getBlog(params.id)
