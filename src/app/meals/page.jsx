b5b5b469c9051492c3638124e5ffb6d64c60797b
abunaim34 <farhanadnanfarabi@gmail.com>
2025-08-18 20:55:28 +0600

    global css

--- src/app/meals/page.jsx
+++ src/app/meals/page.jsx
@@ -2,10 +2,8 @@ import SearchInput from "./Components/SearchInput";
 
 const Meals = async ({ searchParams }) => {
     const query = searchParams
-    console.log(searchParams);
     const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query.search}`)
     const data = await res.json()
-    console.log(data);
     const meals  = data.meals
 
     
@@ -13,7 +11,7 @@ const Meals = async ({ searchParams }) => {
         <div>
             <h1 className="text-5xl">Meals Page</h1>
             <SearchInput />
-            <div className="grid grid-cols-3 gap-4">
+            <div className="grid grid-cols-3 gap-4 bg-color">
                 {
                     meals?.map(meal => <div key={meal.idMeal}>
                         <h1 className="text-xl">{meal.strMeal}</h1>
