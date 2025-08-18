d46a83f204f6c3e7b99b181b80b4729fbc630eeb
abunaim34 <farhanadnanfarabi@gmail.com>
2025-08-18 19:54:05 +0600

    singleData fetching for dynamic blogs

--- src/app/blogs/[id]/page.jsx
+++ src/app/blogs/[id]/page.jsx
@@ -1,8 +1,12 @@
 
-const BlogsDetails = ({params}) => {
+const BlogsDetails = async({params}) => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.id}`);
+    const blog = await res.json() 
     return (
         <div>
             blogs details {params.id}
+            <h1>{blog.title}</h1>
+            <p>{blog.body}</p>
         </div>
     );
 };
