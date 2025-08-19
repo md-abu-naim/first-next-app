17ee283ca55f9790f0987315345229dd11a57fc8
abunaim34 <farhanadnanfarabi@gmail.com>
2025-08-19 21:49:49 +0600

    Delete single data

--- src/app/api/items/[id]/route.js
+++ src/app/api/items/[id]/route.js
@@ -6,6 +6,13 @@ export async function GET(req, {params}) {
   return Response.json({ params: p })
 }
 
+// Delete Data By DELETE
+export async function DELETE(req, {params}) {
+    const p = await params
+    console.log(p);
+ 
+  return Response.json({ params: p })
+}
 
 // Upudate Data By PATCH
 export async function PATCH(req, {params}) {
