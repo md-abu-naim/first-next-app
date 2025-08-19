89cc98d44c46a37ee5ad7221468cc39abae04e00
abunaim34 <farhanadnanfarabi@gmail.com>
2025-08-19 21:26:29 +0600

    api route create

---
+++ src/app/api/items/route.js
@@ -0,0 +1,17 @@
+export async function GET() {
+  
+    const data = {
+        message: 'Successfully get data',
+        error: false,
+        status: 200,
+    }
+ 
+  return Response.json({ data })
+}
+
+export async function POST(req) {
+  
+    const 
+ 
+  return Response.json({ data })
+}
